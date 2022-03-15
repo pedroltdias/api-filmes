b91bd1127f8deffc4791720accd48ba616ddd17e
Pedro Dias <pedroltdias@gmail.com>
2022-03-15 17:31:35 -0300

    Requisicao post para adicionar novo filme

--- api/rotas/filmes/index.js
+++ api/rotas/filmes/index.js
@@ -1,13 +1,19 @@
 const roteador = require('express').Router()
 
-roteador.get('/', (requisicao, resposta) => {
-    const filmes = [
-        { nome: 'Os Vingadores 3'},
-        { nome: 'Destacamento Blood'},
-        { nome: 'Pantera Negra'}
-    ]
+const filmes = [
+    { nome: 'Os Vingadores 3'},
+    { nome: 'Destacamento Blood'},
+    { nome: 'Pantera Negra'}
+]
 
+roteador.get('/', (requisicao, resposta) => {
     resposta.send(JSON.stringify(filmes))
+})
+
+roteador.post('/', (requisicao, resposta) => {      
+    //Adiciona novo filme (validacao)
+    filmes.push(requisicao.body)
+    resposta.send(JSON.stringify(requisicao.body))
 })
 
 module.exports = roteador
